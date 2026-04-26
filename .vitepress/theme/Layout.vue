eb2e764a74fcbc30830a28086090e84f3a83d14a
hxw05 <3374177735@qq.com>
2026-04-26 15:31:52 +0800

    add date

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,20 +1,41 @@
 <script setup>
 import mediumZoom from 'medium-zoom';
-import { onContentUpdated } from 'vitepress';
+import { onContentUpdated, useData } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import { onBeforeUpdate, onMounted, onUpdated } from 'vue';
 const { Layout } = DefaultTheme;
 
 onContentUpdated(() => {
     mediumZoom('img')
-})
+});
+
+const { frontmatter } = useData()
 </script>
 
 <template>
-    <Layout />
+    <Layout>
+        <template #doc-before>
+            <div class="vp-doc-before">
+                <div v-if="frontmatter.date" class="vp-doc-date">
+                    发布于 {{ frontmatter.date }}
+                </div>
+            </div>
+        </template>
+    </Layout>
 </template>
 
 <style lang="css">
+.vp-doc-before {
+    display: flex;
+    gap: 8px;
+    align-items: center;
+    margin-bottom: 5px;
+}
+
+.vp-doc-date {
+    color: var(--vp-c-text-2);
+}
+
 .medium-zoom-overlay {
     z-index: 30;
 }
