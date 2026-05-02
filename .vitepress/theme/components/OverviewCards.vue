3466bf0a1c3aa3f971d5abc0e62e41bdb0b43a63
hxw05 <hxw05@whut.edu.cn>
2026-05-02 16:26:51 +0800

    chore

--- .vitepress/theme/components/OverviewCards.vue
+++ .vitepress/theme/components/OverviewCards.vue
@@ -3,20 +3,6 @@ import stats from '../overall_stats.json'
 </script>
 
 <template>
-  <!-- <div class="overview-cards">
-    <div class="oc-card">
-      <div class="oc-label">总中文字数</div>
-      <div class="oc-value">{{ (stats.charCountChinese / 1000).toFixed(1) }}K</div>
-    </div>
-    <div class="oc-card">
-      <div class="oc-label">总笔记数</div>
-      <div class="oc-value">{{ stats.noteCount }}</div>
-    </div>
-    <div class="oc-card oc-card-wide">
-      <div class="oc-label">最近更新于</div>
-      <div class="oc-value">{{ stats.lastUpdated }}</div>
-    </div>
-  </div> -->
   <ul>
     <li>总中文字数：{{ (stats.charCountChinese / 1000).toFixed(1) }}K（{{(stats.charCountChinese/10000).toFixed(2)}} 万）</li>
     <li>总笔记数：{{ stats.noteCount }} 篇</li>
